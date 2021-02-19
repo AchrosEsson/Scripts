4a2aba10145221e8d9b663339dfcae78f0312c84
AchrosEsson <47488896+AchrosEsson@users.noreply.github.com>
2021-02-19 15:26:50 +0100

    Add files via upload

---
+++ i3/i3wm.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#### Bash-Script zur installation von i3-Gaps ####
+
+echo $'\n'
+echo 'installiere i3-Gaps'
+echo $'\n'
+sleep 2
+
+echo $'\n'
+echo 'Klone Repository...'
+echo $'\n'
+
+git clone https://github.com/maestrogerardo/i3-gaps-deb.git
+
+absolutepath="$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")"
+
+cd $absolutepath/i3-gaps-deb
+
+(. ./i3-gaps-deb)
+
+apt install -y gnome-terminal polybar nitrogen feh rofi compton
+
+mkdir ~/.config/i3/
+mkdir ~/.config/polybar/
+
+cp -r $absolutepath/Scripts/i3/config ~/.config/i3/config
+cp -r $absolutepath/Scripts/i3/polybar-config ~/.config/polybar/config
+
+echo $'\n'
+echo 'i3 wurde erfolgreich installiert, kehre zurück...'
+echo $'\n'
+
+sleep 3
+
+exit
+
