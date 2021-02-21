165a7cb7482d35699928c170676ba9900ca6689f
AchrosEsson <47488896+AchrosEsson@users.noreply.github.com>
2021-02-21 10:21:23 +0100

    Update powermenu.sh

--- polybar/shades/scripts/powermenu.sh
+++ polybar/shades/scripts/powermenu.sh
@@ -1,94 +1,34 @@
-#!/usr/bin/env bash
+#! /bin/bash
 
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
+action=$(yad --width 300 --entry --title "System Logout" \
+    --image=gnome-shutdown \
+    --button="Switch User:2" \
+    --button="gtk-ok:0" --button="gtk-close:1" \
+    --text "Choose action:" \
+    --entry-text \
+    "Power Off" "Reboot" "Suspend" "Logout")
+ret=$?
 
-dir="~/.config/polybar/shades/scripts/rofi"
-uptime=$(uptime -p | sed -e 's/up //g')
+[[ $ret -eq 1 ]] && exit 0
 
-rofi_command="rofi -theme $dir/powermenu.rasi"
+if [[ $ret -eq 2 ]]; then
+    gdmflexiserver --startnew &
+    exit 0
+fi
 
-# Options
-shutdown=" Shutdown"
-reboot=" Restart"
-lock=" Lock"
-suspend=" Sleep"
-logout=" Logout"
-
-# Confirmation
-confirm_exit() {
-	rofi -dmenu\
-		-i\
-		-no-fixed-num-lines\
-		-p "Are You Sure? : "\
-		-theme $dir/confirm.rasi
-}
-
-# Message
-msg() {
-	rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
-}
-
-# Variable passed to rofi
-options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
-
-chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
-case $chosen in
-    $shutdown)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			systemctl poweroff
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
-    $reboot)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			systemctl reboot
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
-    $lock)
-		if [[ -f /usr/bin/i3lock ]]; then
-			i3lock
-		elif [[ -f /usr/bin/betterlockscreen ]]; then
-			betterlockscreen -l
-		fi
-        ;;
-    $suspend)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
-    $logout)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
-				i3-msg exit
-			fi
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
+case $action in
+    Power*) cmd="sudo /sbin/poweroff" ;;
+    Reboot*) cmd="sudo /sbin/reboot" ;;
+    Suspend*) cmd="sudo /bin/sh -c 'echo disk > /sys/power/state'" ;;
+    Logout*) 
+    case $(wmctrl -m | grep Name) in
+        *Openbox) cmd="openbox --exit" ;;
+        *FVWM) cmd="FvwmCommand Quit" ;;
+            *Metacity) cmd="gnome-save-session --kill" ;; 
+        *) exit 1 ;;
+    esac
+    ;;
+    *) exit 1 ;;    
 esac
+
+eval exec $cmd
