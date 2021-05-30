acc7070e3b3347a5ecda5891d6f7e7dddb52cd3c
AchrosEsson <47488896+AchrosEsson@users.noreply.github.com>
2021-05-30 18:22:04 +0200

    Create checkdiskspace.sh

---
+++ checkdiskspace.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+df | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output;
+do
+  echo $output
+  usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
+  partition=$(echo $output | awk '{ print $2}' | sed 's#//192.168.178.100/Proxmox#QNAP#g')
+
+  if [ $usep -ge 90 ]; then
+    echo "Speicherplatz auf <b>$partition</b> bei <b>$usep%</b> <br>von $(hostname) um $(date)" |
+     mail -s "Alert: Speicherplatznutzung bei $usep%" myemail@mail.com
+  fi
+done
