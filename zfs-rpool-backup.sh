21036ec5aabf4d860810f9a09f99bbcb96bfeaaf
AchrosEsson <47488896+AchrosEsson@users.noreply.github.com>
2021-06-24 11:40:38 +0200

    Update zfs-rpool-backup.sh

--- zfs-rpool-backup.sh
+++ zfs-rpool-backup.sh
@@ -14,7 +14,7 @@ new_snap="$source_pool"@"$snapshot_string""$timestamp"
 
 # Create first recursive snapshot of the whole pool.
 zfs snapshot -r "$new_snap"
-# Initial replication via SSH.
+# Initial replication to destination pool.
 zfs send -R "$new_snap" | zfs recv -Fdu "$destination_pool"
 
 # Incremental sends:
@@ -23,7 +23,7 @@ zfs send -R "$new_snap" | zfs recv -Fdu "$destination_pool"
 old_snap=$(zfs list -H -o name -t snapshot -r "$source_pool" | grep "$source_pool"@"$snapshot_string" | tail --lines=1)
 # Create new recursive snapshot of the whole pool.
 zfs snapshot -r "$new_snap"
-# Incremental replication via SSH.
+# Incremental replication to destination pool.
 zfs send -R -I "$old_snap" "$new_snap" | zfs recv -Fdu "$destination_pool"
 # Delete older snaps on the local source (grep -v inverts the selection)
 delete_from=$(zfs list -H -o name -t snapshot -r "$source_pool" | grep "$snapshot_string" | grep -v "$timestamp")
